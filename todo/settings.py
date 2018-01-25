d5ed7406a8fa394fcca09398afa8925d66215a3f
Anna Lidia Błaszczyk <1437346+alblaszczyk@users.noreply.github.com>
2018-01-26 00:00:05 +0100

    Fix static files issues.

--- todo/settings.py
+++ todo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'iz!hb4emddsx7hye9ie8n2v6f)5wkls4=es2wy8vxy%=cflwhg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
